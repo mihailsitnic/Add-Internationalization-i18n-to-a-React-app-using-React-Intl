cabe21a263640858b2cc05b70ea73093086ef9d8
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-12-14 17:36:27 +0200

    01-egghead-react-install-and-configure-the-entry-point-of-react-intl

--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import es from 'react-intl/locale-data/es';
+import fr from 'react-intl/locale-data/fr';
+import messages from './messages';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+addLocaleData([...en, ...es, ...fr]);
+
+let locale =
+  (navigator.languages && navigator.languages[0])
+  || navigator.language
+  || navigator.userLanguage
+  || 'en-US';
+
+ReactDOM.render(
+  <IntlProvider locale={locale} messages={messages[locale]}>
+    <App />
+  </IntlProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
